df09d3e86bccc718cbf321777c498df4762545cc
Saveliy <ya.savchik2000@mail.ru>
2023-07-05 22:56:04 +0400

    Add get_last_transactions function

--- utils.py
+++ utils.py
@@ -1,1 +1,18 @@
+import json
 
+DATA = json.load(open('operations.json'))
+
+
+def get_last_transactions():
+    """
+    Возвращает список из 5 последних выполненных операций
+    """
+    necessary_date = []
+
+    for transaction in DATA:
+        if 'state' not in transaction.keys():
+            continue
+        if transaction['state'] == 'EXECUTED':
+            necessary_date.append(transaction)
+
+    return necessary_date[-5:]
