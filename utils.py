20beec352f68a547dd3c786c47c27009961a6716
Saveliy <ya.savchik2000@mail.ru>
2023-07-06 22:25:41 +0400

    Add transaction_in_class function and fix get_last_transactions

--- utils.py
+++ utils.py
@@ -1,18 +1,53 @@
 import json
+import classes
 
-DATA = json.load(open('operations.json'))
+DATA = json.load(open('operations.json', encoding='utf-8'))
 
 
 def get_last_transactions():
     """
-    Возвращает список из 5 последних выполненных операций
+    Возвращает список из 5 последних выполненных операций, начиная с последней операции
     """
+    # Создаем пустой список для всех выполненных транзакций
     necessary_date = []
-
-    for transaction in DATA:
+    # Проходимся циклом по списку всех транзакций начиная с конца
+    for transaction in reversed(DATA):
+        # Если ключа 'state' нету в словаре (элементе списка), то пропускаем итерацию
         if 'state' not in transaction.keys():
             continue
+        # Если ключ 'state' равен значению 'EXECUTED', то добавляем этот элемент в список выполненных операций
         if transaction['state'] == 'EXECUTED':
             necessary_date.append(transaction)
+    # Возвращаем последние 5 выполненных операций
+    return necessary_date[:5]
 
-    return necessary_date[-5:]
+
+def transaction_in_class(list_):
+    """
+    Принимает список транзакций и возвращает список экземпляров класса Transaction
+    """
+    # Создаем пустой список для будущих экземпляров
+    list_of_classes = []
+
+    # Проходимся циклом по последним транзакцим, задаем аргументы для экземпляра
+    for element in list_:
+        id_ = element['id']
+        state = element['state']
+        date = element['date']
+        amount = element['operationAmount']['amount']
+        name_currency = element['operationAmount']['currency']['name']
+        code_currency = element['operationAmount']['currency']['code']
+        description = element['description']
+        # Если есть счет отправителя
+        if 'from' in element.keys():
+            sender = element['from']
+        else:
+            sender = ''
+        recipient = element['to']
+
+        # Инициализируем экземпляр
+        transaction = classes.Transaction(id_, state, date, amount, name_currency, code_currency, description, sender, recipient)
+        # Добавляем его в список
+        list_of_classes.append(transaction)
+    # Выводим список экземпляров
+    return list_of_classes
