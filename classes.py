60b8c568f47e21a7cd33a698abff6cd299f217a7
Saveliy <ya.savchik2000@mail.ru>
2023-07-09 13:42:47 +0400

    Add hiding_recipient and get_currency. Fix hiding_sender

--- classes.py
+++ classes.py
@@ -18,35 +18,66 @@ class Transaction:
 
     def __repr__(self):
         """
-        Экземпляр возвращает свой id при вызове
+        Возвращает информацию о транзакции в нужном виде
         """
-        return f'Транзакция {self.id}'
+        return f'{self.get_date() + self.description}\n' \
+               f'{self.hiding_sender() + self.hiding_recipient()}\n' \
+               f'{self.get_currency()}\n'
 
     def get_date(self):
         """
         Возвращает дату выполнения транзакции в нужном формате
         """
         date_ = datetime.fromisoformat(self.date)
-        return date_.strftime("%d.%m.%Y")
+        return date_.strftime("%d.%m.%Y ")
 
     def hiding_sender(self):
         """
         Возвращает скрытый номер отправителя
         """
-        # Отделяет номер счета/карты от коментария
+        # Отделяет номер счета/карты от названия карты/счета
         number = ''
+        title = ''
         for symbol in self.sender:
             if symbol.isdigit():
                 number += symbol
+            else:
+                title += symbol
         # Если это номер карты
         if len(number) == 16:
             hiding_card = '** **** '
-            return number[:4] + ' ' + number[4:6] + hiding_card + number[-4:]
+            return title + number[:4] + ' ' + number[4:6] + hiding_card + number[-4:] + ' -> '
+        # Если это номер счета
+        elif len(number) == 20:
+            hiding_account = '**'
+            return title + hiding_account + number[-4:] + ' -> '
+        # Если отправитель отсутствует
+        else:
+            return '-> '
+
+    def hiding_recipient(self):
+        """
+        Возвращает скрытый номер получателя
+        """
+        # Отделяет номер счета/карты от названия карты/счета
+        number = ''
+        title = ''
+        for symbol in self.recipient:
+            if symbol.isdigit():
+                number += symbol
+            else:
+                title += symbol
+        # Если это номер карты
+        if len(number) == 16:
+            hiding_card = '** **** '
+            return title + number[:4] + ' ' + number[4:6] + hiding_card + number[-4:]
         # Если это номер счета
         else:
             hiding_account = '**'
-            return hiding_account + number[-4:]
+            return title + hiding_account + number[-4:]
 
-
-
-
+    def get_currency(self):
+        """
+        Возвращает сумму перевода и название валюты
+        """
+        return self.amount + ' ' + self.name_currency
